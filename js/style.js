43294485366356d5853d78b6d0f487098700fdc8
cidkumagai <f20aa003@gmail.com>
2022-07-15 15:21:28 +0900

    urlテスト6

--- js/style.js
+++ js/style.js
@@ -27,8 +27,8 @@ function init() {
 }
 
 function pageTransition(e) {
-    location.href = `http://kumachen.html.xdomain.jp/CSCP/recipe.html?id=${e.path[0].id}`;
-    // document.location.href = `https://cidkumagai.github.io/CSCP/recipe.html?id=${e.path[0].id}`;
+    // location.href = `http://kumachen.html.xdomain.jp/CSCP/recipe.html?id=${e.path[0].id}`;
+    location.href = `https://cidkumagai.github.io/CSCP/recipe.html?id=${e.path[0].id}`;
 }
 
 function createElement(cookImg, title, comment, id) {    
